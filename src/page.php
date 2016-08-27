6f0b20421db40a318d7119c0ba70714f50466d64
Dan Fletcher <dannyjfletcher@outlook.com>
2016-08-27 23:28:16 +0000

    Add patio depot project page

---
+++ src/page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Page: Home
+ * 
+ */
+ 
+ // Requires/Includes go here.
+ require('../env.php');
+ 
+ // Generate the header markup
+ include('header.php');
+ 
+?>
+
+<!-- TODO: Add page content -->
+        <main>
+        <p>Content goes here!!</p>
+        </main>
+
+<?php 
+
+// Gernerate the footer markup
+include('footer.php') 
+
+?>
+
+
+
