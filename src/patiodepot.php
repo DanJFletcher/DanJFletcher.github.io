51f42311a6485fc8a8f9d66e59047f6eb056258b
Dan Fletcher <dannyjfletcher@outlook.com>
2016-08-27 23:35:53 +0000

    Add project description to patio depot

--- src/patiodepot.php
+++ src/patiodepot.php
@@ -17,26 +17,8 @@
             <section class="project-description">
                 <h1 class="patio-depot-heading">Patio Depot of Destin Florida</h1>
                 <img class="project-thumbnail" src="<?= SITE_URL ?>/images/screenshots/patio_depot_landing_page.jpg" alt="Patio Depot landing page">
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-                magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
-                quis nostrud exerci tation ullamcorper suscipit lobortis nisl
-                ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
-                dolor in hendrerit in vulputate velit esse molestie consequat,
-                vel illum dolore eu feugiat nulla facilisis at vero eros et
-                accumsan et iusto odio dignissim qui blandit praesent luptatum
-                zzril delenit augue duis dolore te feugait nulla facilisi.
-                Nam liber tempor cum soluta nobis eleifend option congue
-                nihil imperdiet doming id quod mazim placerat facer possim
-                assum. Typi non habent claritatem insitam; est usus legentis
-                in iis qui facit eorum claritatem. Investigationes
-                demonstraverunt lectores legere me lius quod ii legunt saepius.
-                Claritas est etiam processus dynamicus, qui sequitur mutationem
-                consuetudium lectorum. Mirum est notare quam littera gothica,
-                quam nunc putamus parum claram, anteposuerit litterarum formas
-                humanitatis per seacula quarta decima et quinta decima. Eodem
-                modo typi, qui nunc nobis videntur parum clari, fiant sollemnes
-                in futurum.</p>
+                
+                <p>This was a fun one! The furniture that this company sells down in Florida is the same furniture that I used to machine and assemble while working for CRP Products Inc. The client accidentally removed their old site before this one was finished - so there was a huge push to get this launched as quickly as possible. After launching, the client made a sale through the site within 30 minutes of going live! It’s making a profit, but there is still a lot of testing and work to be done to get this site to an acceptable level of correctness. Concept and design was done by two of our designers at Redding Designs <a href="#" target="_blank"><em>Corny Bartsch</em></a> and <a href="#" target="_blank"><em>Vanessa Aubin</em></a>. Most of this site was programmed by me, with the help of one of our front-end devs, <a href="#" target="_blank"><em>Frank Weibe</em></a>.</p>
             </section>
         </main>
 
