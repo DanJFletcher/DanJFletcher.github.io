83447b15e09f33b07ddab9c4a654fea9c3bd71b9
Dan Fletcher <dannyjfletcher@outlook.com>
2016-08-28 02:18:20 +0000

    Dist v1.0.0

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
         <link rel="stylesheet" href="<?php echo SITE_URL ?>/vendor/normalize-css/normalize.css" type="text/css" />
         
         <!-- CSS Libs/Frameworks -->
-        <link rel="stylesheet" href="<?php echo SITE_URL ?>/vendor/bootstrap/bootstrap.min.css" type="text/css" />
+        <!--<link rel="stylesheet" href="<?php // echo SITE_URL ?>/vendor/bootstrap/bootstrap.min.css" type="text/css" />-->
         <link rel="stylesheet" href="<?php echo SITE_URL ?>/vendor/owl2/owl.carousel.min.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo SITE_URL ?>/vendor/owl2/owl.theme.default.min.css" type="text/css" />
 
@@ -30,6 +30,8 @@
         <link href="css/stylesheet.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Tangerine">
+        <link href="https://fonts.googleapis.com/css?family=Arvo:400,700" rel="stylesheet">
 
 
     </head>
@@ -50,21 +52,21 @@
             <div id="header-menuholder" class="full">
             	<div class="center">
                 	<div id="header-logo">
-                    	<a href="index.html" title="Home"><img src="images/logos/profile-pic.jpg" alt="Profile"></a>
+                    	<a href="<?= SITE_URL ?>" title="Home"><img src="images/logos/profile-pic.jpg" alt="Profile"></a>
                   </div>
 
                   <div id="header-menu">
                   	<div class="header-menuitem">
-                      	about
+                      	<a href="<?= SITE_URL ?>#about">about</a>
                     </div>
                   	<div class="header-menuitem">
-                      	projects
+                      	<a href="<?= SITE_URL ?>#projects">projects</a>
                     </div>
                   	<div class="header-menuitem">
-                      	testamonies
+                      	<a href="<?= SITE_URL ?>#testamonies">testamonies</a>
                     </div>
                   	<div class="header-menuitem">
-                      	contact
+                      	<a href="<?= SITE_URL ?>#contact">contact</a>
                     </div>
                     <div class="clear"></div>
                   </div> <!-- end #header-menu -->
@@ -76,11 +78,3 @@
               </div>
             </div>
         </header>
-        <main>
-        <section class="about">
-          <h2>About Me.</h2>
-          <hr />
-          <p>Clean. Responsive. Mobile-first. Simple. This is the montra of the modern developer, and words I live by when developing my own projects. Developing clean, elagant sites by day, and studying computer science by night. I'm a developer with a drive to create the best.</p>
-          <a class="btn" href="">Hire Me</a>
-        </section>
-        </main>
--- index.php
+++ index.php
@@ -13,6 +13,45 @@
 ?>
 
 <!-- TODO: Add page content -->
+        <main>
+        <section class="about" id="about">
+          <h2>About Me.</h2>
+          <hr />
+          <p>Clean. Responsive. Mobile-first. Simple. This is the montra of the modern developer, and words I live by when developing my own projects. Developing clean, elagant sites by day, and studying computer science by night. I'm a developer with a drive to create the best.</p>
+          <a class="btn btn-light" href="">Hire Me</a>
+        </section>
+        
+        <div class="padding">
+          <i class="icono-arrow1-down"></i>
+        </div>
+        
+        
+        <section class="work" id="projects">
+          <h2>Showcase.</h2>
+          <hr />
+          <p>Here are some samples of my professional work. Click the "See Project" button to view more details.</p>
+            <div class="owl-carousel owl-theme">
+              <div>
+                <h3>The Family Central Restaruant</h3>
+                <img src="<?= SITE_URL ?>/images/screenshots/family_central_landing_page.png" alt="Family Central Restaruant landing page">
+                <p>Dynamic events calendar and custom event pages.</p>
+                <a href="http://familycentral.ca/" target="_blank" class="btn btn-light">Visit Site</a>
+              </div>
+              <div>
+                <h3>The London Beatles Fest Trivia</h3>
+                <img src="<?= SITE_URL ?>/images/screenshots/beatles_landing_page.png" alt="The London Beatles Festival Trivia landing page">
+                <p>Multiple choice, timed quiz that has user authentication, custom admin dashboard and date-triggered events.</p>
+                <a href="http://trivia.thelondonbeatlesfestival.com/" target="_blank" class="btn btn-light">Visit Site</a>
+              </div>
+              <div>
+                <h3>Patio Depot</h3>
+                <img src="<?= SITE_URL ?>/images/screenshots/patio_depot_landing_page.jpg" alt="Patio Depot landing page">
+                <p>Mobile-first, responsive, website with static conetent.</p>
+                <a href="patiodepot.php" class="btn btn-light">See Project</a>
+              </div>
+            </div>
+        </section>
+        </main>
 
 <?php 
 
