13356c80438db90014d4bfdc774670865ed58063
Dan Fletcher <dannyjfletcher@outlook.com>
2016-08-28 16:28:46 +0000

    Add email link

--- src/index.php
+++ src/index.php
@@ -18,7 +18,7 @@
           <h2>About Me.</h2>
           <hr />
           <p>Clean. Responsive. Mobile-first. Simple. This is the montra of the modern developer, and words I live by when developing my own projects. Developing clean, elagant sites by day, and studying computer science by night. I'm a developer with a drive to create the best.</p>
-          <a class="btn btn-light" href="">Hire Me</a>
+          <a href="mailto:dan@danjfletcher.ca" class="btn btn-light" href="">Email Me</a>
         </section>
         
         <div class="padding">
