d907650c39fcea8df4c480d4becaf3bb464675c3
Dan Fletcher <dannyjfletcher@outlook.com>
2016-08-28 17:49:21 +0000

    Change size of showcase styles

--- src/index.php
+++ src/index.php
@@ -33,25 +33,25 @@
           <p>Here are some samples of my professional work as well as some of my side projects. Click the "See Project" button to view more details.</p>
             <div class="owl-carousel owl-theme">
               <div>
-                <h3>The Family Central Restaruant</h3>
+                <h3 class="showcase-title">The Family Central Restaruant</h3>
                 <img src="<?= SITE_URL ?>/images/screenshots/family_central_landing_page.png" alt="Family Central Restaruant landing page">
                 <p>Dynamic events calendar and custom event pages.</p>
                 <a href="http://familycentral.ca/" target="_blank" class="btn btn-light">Visit Site</a>
               </div>
               <div>
-                <h3>The London Beatles Fest Trivia</h3>
+                <h3 class="showcase-title">The London Beatles Fest Trivia</h3>
                 <img src="<?= SITE_URL ?>/images/screenshots/beatles_landing_page.png" alt="The London Beatles Festival Trivia landing page">
                 <p>Multiple choice, timed quiz that has user authentication, custom admin dashboard and date-triggered events.</p>
                 <a href="http://trivia.thelondonbeatlesfestival.com/" target="_blank" class="btn btn-light">Visit Site</a>
               </div>
               <div>
-                <h3>Patio Depot</h3>
+                <h3 class="showcase-title">Patio Depot</h3>
                 <img src="<?= SITE_URL ?>/images/screenshots/patio_depot_landing_page.jpg" alt="Patio Depot landing page">
                 <p>Mobile-first, responsive, website with static conetent.</p>
                 <a href="patiodepot.php" class="btn btn-light">See Project</a>
               </div>
               <div>
-                <h3>Electronic Calculator</h3>
+                <h3 class="showcase-title">Electronic Calculator</h3>
                 <img src="<?= SITE_URL ?>/images/screenshots/electronic_calculator.jpg" alt="Electronic Calculator">
                 <p>Web app simulating a simple desktop calculator.</p>
                 <a href="//danjfletcher.github.io/electronic-calculator/" class="btn btn-light">See Project</a>
